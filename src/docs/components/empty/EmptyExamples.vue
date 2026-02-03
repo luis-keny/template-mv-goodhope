771dae2f4759df50b529cf16aaba5029aca7422b
luis-keny <luis.lucero03@outlook.com>
2026-02-03 10:53:45 -0500

    docs(empty): add documentation page with example and API

---
+++ src/docs/components/empty/EmptyExamples.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ArrowUpRightIcon, FolderCode } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+</script>
+
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>Empty State</CardTitle>
+      <CardDescription>
+        Muestra un estado vacío cuando no hay datos para mostrar.
+      </CardDescription>
+    </CardHeader>
+    <CardContent class="flex justify-center">
+        <Empty>
+            <EmptyHeader>
+            <EmptyMedia variant="icon">
+                <FolderCode />
+            </EmptyMedia>
+            <EmptyTitle>No Projects Yet</EmptyTitle>
+            <EmptyDescription>
+                You haven't created any projects yet. Get started by creating your first
+                project.
+            </EmptyDescription>
+            </EmptyHeader>
+            <EmptyContent>
+            <div class="flex gap-2">
+                <Button>Create Project</Button>
+                <Button variant="outline">
+                Import Project
+                </Button>
+            </div>
+            </EmptyContent>
+            <Button variant="link" as-child class="text-muted-foreground" size="sm">
+            <a href="#">
+                Learn More <ArrowUpRightIcon />
+            </a>
+            </Button>
+        </Empty>
+    </CardContent>
+  </Card>
+</template>
