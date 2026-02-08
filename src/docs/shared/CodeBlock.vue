b71f233b239dee126d8dac8b956fd4e4e119a681
luis-keny <luis.lucero03@outlook.com>
2026-02-08 13:04:49 -0500

    feat(code-block): add copy functionality and tooltip to code block component

--- src/docs/shared/CodeBlock.vue
+++ src/docs/shared/CodeBlock.vue
@@ -2,6 +2,9 @@
 import { ref, computed, watchEffect } from 'vue'
 import hljs from 'highlight.js/lib/common'
 import 'highlight.js/styles/atom-one-dark.css'
+import { CheckCheck, Copy } from 'lucide-vue-next';
+import { Button } from '@/components/ui/button';
+import { Tooltip, TooltipTrigger, TooltipProvider, TooltipContent } from '@/components/ui/tooltip';
 
 const props = withDefaults(defineProps<{
   code: string
@@ -33,15 +36,44 @@ watchEffect(() => {
     html.value = hljs.highlightAuto(normalizedCode.value).value
   }
 })
+
+const copied = ref(false)
+const copy = async () => {
+  await navigator.clipboard.writeText(props.code)
+  copied.value = true
+  setTimeout(() => (copied.value = false), 1200)
+}
 </script>
 
 <template>
-  <div class="hljs rounded-md bg-black/90 p-4 text-xs leading-relaxed overflow-x-auto">
+  <div class="relative hljs rounded-md bg-black/90 p-4 text-xs leading-relaxed">
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger as-child>
+          <Button variant="ghost" size="icon-sm" @click="copy" class="absolute top-2 right-2 text-white/50 hover:bg-white/10 hover:text-white">
+            <span class="sr-only">Copy</span>
+            <span v-if="!copied">
+              <Copy/>
+            </span>
+            <span v-else>
+              <CheckCheck/>
+            </span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p v-if="!copied">Copiar código</p>
+          <p v-else>Copiado</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+    
     <div class="flex">
       <div v-if="showLineNumbers" aria-hidden="true" class="mr-4 select-none text-right text-neutral-400">
         <span class="block" v-for="n in lineCount" :key="n">{{ n }}</span>
       </div>
-      <pre class="m-0" v-html="html"></pre>
+      <div class="overflow-x-auto no-scrollbar">
+        <pre class="m-0" v-html="html"></pre>
+      </div>
     </div>
   </div>
 </template>
